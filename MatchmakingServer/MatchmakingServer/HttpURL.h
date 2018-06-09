7844c32207f9b50309e753d6c1f8f7ee8eb61648
진현수 <jhs9253@naver.com>
2018-06-10 03:00:23 +0900

    JSON 클래스 추가

--- MatchmakingServer/MatchmakingServer/HttpURL.h
+++ MatchmakingServer/MatchmakingServer/HttpURL.h
@@ -1,9 +1,22 @@
-#ifndef _MATCHINGSERVER_LIB_URL_H_
-#define _MATCHINGSERVER_LIB_URL_H_
+#ifndef _MATCHINGSERVER_LIB_HTTP_H_
+#define _MATCHINGSERVER_LIB_HTTP_H_
 
 //	#define CURL_STATICLIB
 
-#include "../curl/include/curl.h"
+#include <cpprest\http_client.h>
+#include <cpprest\filestream.h>
+
+//	#include "../curl/include/curl.h"		//	CURL lib 이용할 경우
+
+#pragma comment(lib, "cpprest120_2_4")		// Windows Only
+using namespace utility;                    // Common utilities like string conversions
+using namespace web;                        // Common features like URIs.
+using namespace web::http;                  // Common HTTP functionality
+using namespace web::http::client;          // HTTP client features
+using namespace concurrency::streams;       // Asynchronous streams
+
+
+
 
 
 
--- MatchmakingServer/MatchmakingServer/Json.h
+++ MatchmakingServer/MatchmakingServer/Json.h
@@ -11,6 +11,40 @@ StringBuffer StringJSON;
 Writer<StringBuffer, UTF16<>> writer(StringJSON);
 
 
+#include <cpprest\json.h>
+
+class CJsonObject
+{
+public:
+	CJsonObject();
+	CJsonObject(web::json::value _pValue, std::vector<std::wstring> _pKeyList);
+
+	bool	hasStr()	{ return !_StrList.empty(); }
+	bool	hasInt()	{ return !_IntList.empty(); }
+	bool	hasDbl()	{ return !_DblList.empty(); }
+	bool	hasBool()	{ return !_BoolList.empty(); }
+	bool	isArray()	{ return !_vArrayList.empty(); }
+
+	std::wstring	GetString(std::wstring _pKey)	{ return _StrList.at(_pKey); }
+	int				GetInt(std::wstring _pKey)		{ return _IntList.at(_pKey); }
+	double			GetDouble(std::wstring _pKey)	{ return _DblList.at(_pKey); }
+	bool			GetBool(std::wstring _pKey)		{ return _BoolList.at(_pKey); }
+
+	void Clear();
+	web::json::value GetJsonValue();
+
+	void Print();
+
+public:
+	std::unordered_map<std::wstring, std::wstring>	_StrList;
+	std::unordered_map<std::wstring, int>			_IntList;
+	std::unordered_map<std::wstring, double>		_DblList;
+	std::unordered_map<std::wstring, bool>			_BoolList;
+	std::vector<CJsonObject>						_vArrayList;
+	std::vector<std::wstring>						_vKeyList;
+};
 
 
-#endif
+
+
+#endif _MATCHINGSERVER_LIB_JSON_H_
